01d31f5fd6b32f84b554466f55fb67200e06117f
DatNtPH51025 <datntph51025@gmail.com>
2025-06-28 22:07:13 +0700

    CRUD EXERCISE

--- Sever/FitlifeBackend/src/app.js
+++ Sever/FitlifeBackend/src/app.js
@@ -10,6 +10,9 @@ const sleepRouter = require("./routers/sleepRouter");
 const runRouter = require("./routers/runRouter");
 const gpsRouter = require("./routers/gpsRouter");
 const stepRouter = require("./routers/stepRouter");
+const exerciseRouter = require("./routers/exerciseRouter");
+const userExerciseRouter = require("./routers/userExerciseRouter");
+
 
 dotenv.config();
 const app = express();
@@ -24,7 +27,8 @@ app.use("/api/sleeps", sleepRouter);
 app.use("/api/runs", runRouter);
 app.use("/api/gps", gpsRouter);
 app.use("/api/steps", stepRouter);
-
+app.use("/api/exercises", exerciseRouter);
+app.use("/api/user-exercises", userExerciseRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'API route not found' });
@@ -33,5 +37,6 @@ app.use((req, res) => {
 app.use(errorMiddleware);
 
 module.exports = app;
+
 
 
---
+++ Sever/FitlifeBackend/src/controllers/exerciseController.js
@@ -0,0 +1,41 @@
+const Exercise = require("../models/exerciseModel");
+
+// Tạo bài tập mới
+exports.createExercise = async (req, res) => {
+    try {
+        const exercise = await Exercise.create(req.body);
+        res.status(201).json(exercise);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Lấy danh sách bài tập
+exports.getExercises = async (req, res) => {
+    try {
+        const exercises = await Exercise.find();
+        res.json(exercises);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Cập nhật bài tập
+exports.updateExercise = async (req, res) => {
+    try {
+        const updated = await Exercise.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updated);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Xoá bài tập
+exports.deleteExercise = async (req, res) => {
+    try {
+        await Exercise.findByIdAndDelete(req.params.id);
+        res.status(204).send();
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
