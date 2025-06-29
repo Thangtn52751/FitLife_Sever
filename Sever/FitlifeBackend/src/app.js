acdfe1c0d064a636e06408666551fd02665cb3d2
PH53089 <anhptph53089@gmail.com>
2025-06-29 23:02:54 +0700

    add diary feature

--- Sever/FitlifeBackend/src/app.js
+++ Sever/FitlifeBackend/src/app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const errorMiddleware = require('../src/middlewares/errorMiddleware');
 const authRouter = require('../src/routers/authRouter');
 const userRouter = require('../src/routers/userRouter');
+const diaryRouter = require('./routers/diaryRouter');
 
 dotenv.config();
 const app = express();
@@ -16,11 +17,20 @@ app.use(morgan('dev'));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
+app.use('/api/diaries', diaryRouter);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'API route not found' });
 });
 
+
+
+
 app.use(errorMiddleware);
+
+
+
+
 
 module.exports = app;
