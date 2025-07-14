75d0996dc4e33a4de06be0964f05dcbcb1f4dcbb
DatNtPH51025 <datntph51025@gmail.com>
2025-07-14 15:46:31 +0700

    final

--- Sever/FitlifeBackend/src/controllers/exerciseController.js
+++ Sever/FitlifeBackend/src/controllers/exerciseController.js
@@ -4,51 +4,10 @@ const ExerciseRound = require("../models/exerciseRoundModel");
 // Tạo bài tập kèm rounds
 exports.createExercise = async (req, res) => {
     try {
-<<<<<<< HEAD
         const exercise = await Exercise.create(req.body);
         res.status(201).json(exercise);
     } catch (err) { 
-=======
-        const {
-            title,
-            description,
-            durationMin,
-            calories,
-            imageUrl,
-            videoUrl,
-            level,
-            rounds, // 👈 lấy rounds từ body nếu có
-        } = req.body;
 
-        // Bước 1: tạo exercise
-        const exercise = await Exercise.create({
-            title,
-            description,
-            durationMin,
-            calories,
-            imageUrl,
-            videoUrl,
-            level,
-        });
-
-        // Bước 2: nếu có rounds, tạo từng round kèm theo exerciseId
-        if (Array.isArray(rounds) && rounds.length > 0) {
-            const roundDocs = rounds.map((round, index) => ({
-                title: round.title,
-                durationSec: round.durationSec,
-                order: round.order || index + 1,
-                exerciseId: exercise._id,
-            }));
-
-            await ExerciseRound.insertMany(roundDocs);
-        }
-
-        res.status(201).json({
-            message: "Tạo bài tập và các round thành công",
-            exerciseId: exercise._id,
-        });
-    } catch (err) {
->>>>>>> a032d0142515bf078108bc983653f04458645b42
         res.status(400).json({ error: err.message });
     }
 };
